7c30457d7817a29f86dbd990af30e3ea3c2bc008
sumanth aavula <sumanthaavula@gmail.com>
2020-04-08 16:12:07 +0530

    modified

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -11,9 +11,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-04ac550b78324f651"
+    us-west-2 = "ami-02e30ba14d8ffa6e6"
+    eu-west-1 = "ami-0798ac7e2b0fb9e75"
   }
 }
 
